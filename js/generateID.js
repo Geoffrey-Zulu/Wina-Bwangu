fa363745d5942a1e2c78a7eb7599dd2ce424cb64
Geoffrey-Zulu <76912380+Geoffrey-Zulu@users.noreply.github.com>
2023-10-01 15:18:24 +0200

    Last

--- js/generateID.js
+++ js/generateID.js
@@ -1,17 +1,17 @@
 // Function to generate a random Transaction ID
 function ID() {
-    const prefix = "WB"; // Text prefix for the Transaction ID
-    const minNumber = 1; // Minimum number value
-    const maxNumber = 9999999; // Maximum number value (7 digits)
+  const prefix = "WB"; // Text prefix for the Transaction ID
+  const minNumber = 1; // Minimum number value
+  const maxNumber = 9999999; // Maximum number value (7 digits)
 
-    // Generate a random number within the specified range
-    const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  // Generate a random number within the specified range
+  const randomNumber =
+    Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
-    // Create the Transaction ID by combining the prefix and random number
-    const transactionID = `${prefix}${String(randomNumber).padStart(7, '0')}`;
+  // Create the Transaction ID by combining the prefix and random number
+  const transactionID = `${prefix}${String(randomNumber).padStart(7, "0")}`;
 
-    // Display the Transaction ID in the specified HTML element
-    const generate = document.getElementById("ID");
-    generate.innerHTML = "Transaction ID:" + transactionID;
+  // Display the Transaction ID in the specified HTML element
+  const generate = document.getElementById("ID");
+  generate.innerHTML = "Transaction ID:" + transactionID;
 }
-
